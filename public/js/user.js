524c2d5b4932d3ae91ef47faf0a643ed006f1d39
lukephelan <luke.phelan@hotmail.com>
2016-07-05 10:58:39 +1000

    Prepared routes to obtain the data from a heroku-hosted API

--- public/js/user.js
+++ public/js/user.js
@@ -1,9 +1,9 @@
 var UserProfile = Backbone.Model.extend({
-	urlRoot: 'http://localhost:8080/api/profiles'
+	urlRoot: 'https://historic-kings-canyon-91668.herokuapp.com/api/profiles'
 })
 
 // You have to ,anually account for pluralization of model.
 var UserProfiles = Backbone.Collection.extend({
 	model: UserProfile,
-	url: 'http://localhost:8080/api/profiles'
+	url: 'https://historic-kings-canyon-91668.herokuapp.com/api/profiles'
 })
